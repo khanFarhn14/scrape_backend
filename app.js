ed508021910e97517f056b893b79f61110aa5922
Furqan Khan <furqankhan5403@gmail.com>
2023-11-26 19:07:21 +0530

    added body parser and cart route

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const connectDB = require('./db/connection')
 const product = require('./routes/product')
+const bodyParser = require('body-parser')
+const users = require('./routes/user')
 const notFound = require('./middleware/notFound')
+const cartItems = require('./routes/cart')
 
 require('dotenv').config();
 
 
 const app  = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use('/api/v1', product)
+app.use('/api/v1', users)
+app.use('/api/v1', cartItems)
 app.use(notFound)
 
 
