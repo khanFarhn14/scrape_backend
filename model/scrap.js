9366d3348d139aa438969c7d21077a3ed34b275b
Furqan Khan <furqankhan5403@gmail.com>
2023-11-27 15:17:48 +0530

    added scrap model

---
+++ model/scrap.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const scrapSchema = new mongoose.Schema({
+    scrapName:{type:String, required: true},
+    scrapPrice:{type:Number, required: true},
+    scrapImage:{type: String, required: true},
+    scrapCategory:{type: String, required: true},
+}, {collection: 'scrap'})
+
+module.exports = new mongoose.model('scrap', scrapSchema);
