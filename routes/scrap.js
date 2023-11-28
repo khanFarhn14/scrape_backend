6c78e4b874b90632d37051db76bc56bd0f028bdd
Furqan Khan <furqankhan5403@gmail.com>
2023-11-28 22:31:44 +0530

    added separate routes for category

--- routes/scrap.js
+++ routes/scrap.js
@@ -1,6 +1,10 @@
 const express = require('express')
 
 const router = express.Router()
-const { scrapList } = require('../controllers/scrap');
-router.route('/allscrap').get(scrapList);
+const { scrapList, wires, eWastes, metals, papers } = require('../controllers/scrap');
+router.route('/scrap/all').get(scrapList);
+router.route('/scrap/wires').get(wires);
+router.route('/scrap/e-wastes').get(eWastes);
+router.route('/scrap/metals').get(metals);
+router.route('/scrap/papers').get(papers);
 module.exports = router
