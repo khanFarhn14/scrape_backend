e35edd2875f2b386200d0e124409122d1fc08c1a
Furqan Khan <furqankhan5403@gmail.com>
2023-11-30 23:45:49 +0530

    added model for orders

---
+++ model/order.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+    scrapName:{type:String, required: true},
+    scrapImage:{type:String, required: true},
+    weight:{type:String, required:true}, 
+    requestStatus: {type:String, required:true},
+    requestDate: {type:String, required:true},
+})
+
+const orderSchema = new mongoose.Schema({
+    mobile:{type:Number, required: true, unique:true},
+    orders: [productSchema]
+
+}, {collection: 'Order'})
+
+module.exports = new mongoose.model('orders', orderSchema);
