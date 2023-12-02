81b4701f520049ae0719cf8ec567e005fde113f7
Furqan Khan <furqankhan5403@gmail.com>
2023-12-02 23:55:57 +0530

    added order routes for user and admin

--- routes/order.js
+++ routes/order.js
@@ -1,11 +1,12 @@
 const express = require('express')
 
 const router = express.Router()
-const { orderHistory, addOrder, recentOrders, updateRequestStatus } = require('../controllers/order');
+const { orderHistory, addOrder, recentOrders, updateRequestStatus, ordersFromUser } = require('../controllers/order');
 router.route('/user/:mobile/history').get(orderHistory);
 router.route('/user/:mobile/recent').get(recentOrders);
 router.route('/user/:mobile/addorder').post(addOrder);
 router.route('/user/:mobile/addorder').post(addOrder);
 router.route('/admin/statusupdate').patch(updateRequestStatus);
+router.route('/admin/allorders').get(ordersFromUser);
 
 module.exports = router
