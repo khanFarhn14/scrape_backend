de8798e9f09c1bc8f7596d0110c5d69841907c0a
Furqan Khan <furqankhan5403@gmail.com>
2023-12-06 00:42:43 +0530

    added clearcarlist route

--- routes/cart.js
+++ routes/cart.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
 const router = express.Router()
-const { CartItems, addItem, deleteItem } = require('../controllers/cart');
-router.route('/user/:mobile/cart').get(CartItems).put(addItem).delete(deleteItem);
+const { CartItems, addItem, deleteItem, clearCartList } = require('../controllers/cart');
+router.route('/user/:mobile/cart').get(CartItems).put(addItem).delete(deleteItem)
+router.route('/user/:mobile/cart/clearlist').delete(clearCartList);
 module.exports = router
