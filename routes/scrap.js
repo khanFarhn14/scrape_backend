9e13eb19b11d9c669457f9750630fba3a36fccc5
Furqan Khan <furqankhan5403@gmail.com>
2023-12-08 20:13:11 +0530

    added get singleScrap Route

--- routes/scrap.js
+++ routes/scrap.js
@@ -1,10 +1,11 @@
 const express = require('express')
 
 const router = express.Router()
-const { scrapList, wires, eWastes, metals, papers } = require('../controllers/scrap');
+const { scrapList, wires, eWastes, metals, papers, singleScrap } = require('../controllers/scrap');
 router.route('/scrap/all').get(scrapList);
 router.route('/scrap/wires').get(wires);
 router.route('/scrap/e-wastes').get(eWastes);
 router.route('/scrap/metals').get(metals);
 router.route('/scrap/papers').get(papers);
+router.route('/scrap/:id').get(singleScrap);
 module.exports = router
