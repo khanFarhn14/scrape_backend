e73df2bc47763d3fcbaf36da43c114b69e41c8b7
Furqan Khan <furqankhan5403@gmail.com>
2023-12-09 16:24:21 +0530

    added updateScrapPrice route

--- routes/scrap.js
+++ routes/scrap.js
@@ -1,11 +1,12 @@
 const express = require('express')
 
 const router = express.Router()
-const { scrapList, wires, eWastes, metals, papers, singleScrap } = require('../controllers/scrap');
+const { scrapList, wires, eWastes, metals, papers, singleScrap, updateScrapPrice } = require('../controllers/scrap');
 router.route('/scrap/all').get(scrapList);
 router.route('/scrap/wires').get(wires);
 router.route('/scrap/e-wastes').get(eWastes);
 router.route('/scrap/metals').get(metals);
 router.route('/scrap/papers').get(papers);
 router.route('/scrap/:id').get(singleScrap);
+router.route('/scrap/:id').patch(updateScrapPrice);
 module.exports = router
