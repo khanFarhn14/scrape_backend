bee19d9fc42c5be0f3a3b7dbc7eb8957a7075ed8
Furqan Khan <furqankhan5403@gmail.com>
2023-12-09 16:23:35 +0530

    ädded scrapPrice in model

--- model/order.js
+++ model/order.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const productSchema = new mongoose.Schema({
     scrapName:{type:String, required: true},
     scrapImage:{type:String, required: true},
-    scrapWeight:{type:String, required:true}, 
+    scrapWeight:{type:String, required:true},
+    scrapPrice:{type:String, required:true},
     requestStatus: {type:String, required:true},
     requestDate: {type:String, required:true},
     confirmationDate: {type:String, required:true},
