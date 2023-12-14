24943d6cceb8d0227c68b27046e97b5b7282281d
Furqan Khan <furqankhan5403@gmail.com>
2023-12-14 18:41:35 +0530

    changed routes for recent order and order history

--- routes/order.js
+++ routes/order.js
@@ -2,8 +2,8 @@ const express = require('express')
 
 const router = express.Router()
 const { orderHistory, addOrder, recentOrders, updateRequestStatus, ordersFromUser } = require('../controllers/order');
-router.route('/user/:mobile/history').get(orderHistory);
-router.route('/user/:mobile/recent').get(recentOrders);
+router.route('/user/:mobile/order/history').get(orderHistory);
+router.route('/user/:mobile/order/recent').get(recentOrders);
 router.route('/user/:mobile/addorder').post(addOrder);
 router.route('/admin/statusupdate').patch(updateRequestStatus);
 router.route('/admin/allorders').get(ordersFromUser);
