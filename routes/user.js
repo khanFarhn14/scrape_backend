f2c86797a1a60edd87b11a326848a010ce73c7ee
Furqan Khan <furqankhan5403@gmail.com>
2023-12-14 18:42:33 +0530

    updated user details and update user route

--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,5 @@ const router = express.Router()
 const {createUser, updateUser, getAllUsers, userDetails}= require('../controllers/user')
 
 router.route('/users').get(getAllUsers).post(createUser)
-router.route('/users/:mobile').get(userDetails).patch(updateUser);
+router.route('/user/:mobile').get(userDetails).patch(updateUser);
 module.exports = router
