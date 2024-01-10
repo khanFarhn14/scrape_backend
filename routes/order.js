3cc2c9f6d51a5f215183e41517f0bcc03aa05cd1
Furqan Khan <furqankhan5403@gmail.com>
2024-01-10 13:16:56 +0530

    added routes for settled orders and pending orders for admin

--- routes/order.js
+++ routes/order.js
@@ -1,11 +1,12 @@
 const express = require('express')
 
 const router = express.Router()
-const { orderHistory, addOrder, recentOrders, updateRequestStatus, ordersFromUser } = require('../controllers/order');
+const { orderHistory, addOrder, recentOrders, updateRequestStatus, ordersFromUser, pendingOrdersFromUser } = require('../controllers/order');
 router.route('/user/:mobile/order/history').get(orderHistory);
 router.route('/user/:mobile/order/recent').get(recentOrders);
 router.route('/user/:mobile/addorder').post(addOrder);
 router.route('/admin/statusupdate').patch(updateRequestStatus);
-router.route('/admin/allorders').get(ordersFromUser);
+router.route('/admin/settledorders').get(ordersFromUser);
+router.route('/admin/pendingorders').get(pendingOrdersFromUser);
 
 module.exports = router
