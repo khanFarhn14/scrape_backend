7963ad705b8b6062bb685590dc6cc0c94afe28f6
Furqan Khan <furqankhan5403@gmail.com>
2024-01-10 20:32:50 +0530

    added route for delete account

--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,10 @@ const express = require('express')
 
 const router = express.Router()
 
-const {createUser, updateUser, getAllUsers, userDetails}= require('../controllers/user')
+const {createUser, updateUser, getAllUsers, userDetails, deleteUser}= require('../controllers/user')
 
 router.route('/users').post(createUser);
 router.route('/user/:mobile').get(userDetails).patch(updateUser);
 router.route('/admin/allusers').get(getAllUsers);
+router.route('/user/deleteaccount').delete(deleteUser);
 module.exports = router
