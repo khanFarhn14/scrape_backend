11095479e015863cf07fece3bb17ebe4c5f3403d
Furqan Khan <furqankhan5403@gmail.com>
2024-01-14 16:54:09 +0530

    changed route for create user

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 const {createUser, updateUser, getAllUsers, userDetails, deleteUser}= require('../controllers/user')
 
-router.route('/users').post(createUser);
+router.route('/user').post(createUser);
 router.route('/user/:mobile').get(userDetails).patch(updateUser);
 router.route('/admin/allusers').get(getAllUsers);
 router.route('/user/deleteaccount').delete(deleteUser);
