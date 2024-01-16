8ef94c5f9c3b74151161bf669558cef7abd87e7b
Furqan Khan <furqankhan5403@gmail.com>
2024-01-16 12:00:04 +0530

    added markdelivered route

--- routes/order.js
+++ routes/order.js
@@ -1,12 +1,13 @@
 const express = require('express')
 
 const router = express.Router()
-const { orderHistory, addOrder, recentOrders, updateRequestStatus, ordersFromUser, pendingOrdersFromUser } = require('../controllers/order');
+const { orderHistory, addOrder, recentOrders, updateRequestStatus, ordersFromUser, pendingOrdersFromUser, markDelivered } = require('../controllers/order');
 router.route('/user/:mobile/order/history').get(orderHistory);
 router.route('/user/:mobile/order/recent').get(recentOrders);
 router.route('/user/:mobile/addorder').post(addOrder);
 router.route('/admin/statusupdate').patch(updateRequestStatus);
 router.route('/admin/settledorders').get(ordersFromUser);
 router.route('/admin/pendingorders').get(pendingOrdersFromUser);
+router.route('/admin/delivered').patch(markDelivered);
 
 module.exports = router
