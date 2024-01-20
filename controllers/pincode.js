1a0f73980040d40996c03d429a4067d2bf9f4594
Furqan Khan <furqankhan5403@gmail.com>
2024-01-20 17:40:47 +0530

    created addpincode api function

---
+++ controllers/pincode.js
@@ -0,0 +1,33 @@
+const Pincode = require('../model/pincode')
+
+const addPincode = async (req, res) =>{
+    const {pincode, postalOffice} = req.body;
+
+    try {
+        // Check if the pincode already exists
+        const existingPincode = await Pincode.findOne({ pincode });
+
+        if (existingPincode) {
+            res.status(400).json({ error: 'Pincode already exists' });
+        }
+
+        // Create a new pincode
+        const newPincode = new Pincode({
+            pincode,
+            postalOffice,
+        });
+
+        // Save the new pincode to the database
+        await newPincode.save();
+        res.status(201).json({ message: 'Pincode added successfully' });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: 'Internal Server Error'})
+    }
+}
+
+
+module.exports = {
+    addPincode
+}
