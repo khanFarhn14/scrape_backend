450ed71ce0c8326087160d8e383d611ff2d00a43
Furqan Khan <furqankhan5403@gmail.com>
2024-01-20 20:55:13 +0530

    added routes for delete and get pincodes

--- routes/pincode.js
+++ routes/pincode.js
@@ -1,7 +1,9 @@
 const express = require('express')
 
 const router = express.Router()
-const { addPincode } = require('../controllers/pincode')
+const { addPincode, deletePincode, getPincodes } = require('../controllers/pincode')
 
 router.route('/admin/addpincode').post(addPincode)
+router.route('/admin/deletepincode').delete(deletePincode)
+router.route('/admin/pincodes').get(getPincodes)
 module.exports = router
